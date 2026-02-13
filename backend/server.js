76c085e6f3e5a25b0d53fc1db4514bbaca75d4df
Shreyansh Singh <singh.shreyansh1756@gmail.com>
2026-02-13 18:49:30 +0530

    prep for deployement

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Ryze AI Backend Running" });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT ||5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}
-    http://localhost:5000`);
+  console.log(`Server running on port ${PORT}`);
 });
